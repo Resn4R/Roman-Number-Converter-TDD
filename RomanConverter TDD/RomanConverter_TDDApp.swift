1569e8dcedc75969d683ab232fb2c45101079bd0
Resn4R <purview-joust.0x@icloud.com>
2023-11-16 17:08:10 +0000

    Initial Commit

---
+++ RomanConverter TDD/RomanConverter_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RomanConverter_TDDApp.swift
+//  RomanConverter TDD
+//
+//  Created by Vito Borghi on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RomanConverter_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
