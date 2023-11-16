5c46cbe49362bdbed0a477101eaa917d50aa3a4a
Resn4R <purview-joust.0x@icloud.com>
2023-11-16 17:12:22 +0000

    one test passed

---
+++ RomanConverter TDD/Converter.swift
@@ -0,0 +1,15 @@
+//
+//  Converter.swift
+//  RomanConverter TDD
+//
+//  Created by Vito Borghi on 16/11/2023.
+//
+
+import Foundation
+
+struct Converter {
+    
+    func getRomanNumber(of number: Int) -> String {
+        return "I"
+    }
+}
