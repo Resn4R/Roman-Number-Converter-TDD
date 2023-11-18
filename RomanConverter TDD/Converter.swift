8f9c95d5a80db2c57cbabdf9ad59d1e8c2a7cc47
Resn4R <purview-joust.0x@icloud.com>
2023-11-18 14:01:02 +0000

    seven tests passed, code refactored

--- RomanConverter TDD/Converter.swift
+++ RomanConverter TDD/Converter.swift
@@ -9,41 +9,35 @@ import Foundation
 
 struct Converter {
     
-    let referenceTable: [Int: String] = [
-        1: "I",
-        2: "II",
-        3: "III",
-        4: "IV",
-        5: "V",
-        9: "IX",
-        10: "X",
-        40: "XL",
-        50: "L",
-        90: "XC",
-        100: "C",
-        400: "CD",
-        500: "D",
-        900: "CM",
-        1000: "M"
+    static let reference: [(num: Int, char: String)] = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
     ]
     
     func getRomanNumber(of number: Int) -> String {
+        
         var result = ""
-
-        if referenceTable.contains(where: { (key: Int, _) in number == key }) {
-            result = referenceTable[number]!
-        } else {
-            while number.isMultiple(of: 10) {
-                var rest = number % 10
-                var dividend = number / 10
-                
-                if rest > dividend {
-                    result += referenceTable[dividend] ?? ""
-                    result += referenceTable[rest] ?? ""
-                }
+        var remainder = number
+        
+        Self.reference.forEach { entry in
+            while remainder >= entry.num {
+                result += entry.char
+                remainder -= entry.num
             }
         }
         
         return result
+        
     }
 }
